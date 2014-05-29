7ce55d92e60d42262ce7911b1a26f8677febc1d5
lkrjunior <lkrjunior@terra.com.br>
2014-05-28 21:01:48 -0300

    Trabalho 6

--- Arvore_AVL_RB/src/app/ArvoreApp.java
+++ Arvore_AVL_RB/src/app/ArvoreApp.java
@@ -6,20 +6,7 @@ public class ArvoreApp
 {
 
 	public static void main(String[] args) 
-	{
-		
-		/*
-		ArvoreRedBlack<String, Integer> st = new ArvoreRedBlack<String, Integer>();
-        for (int i = 0; !StdIn.isEmpty(); i++) 
-		{
-            String key = StdIn.readString();
-            st.put(key, i);
-        }
-        for (String s : st.keys())
-            StdOut.println(s + " " + st.get(s));
-        StdOut.println();
-		*/
-		
+	{	
 		ArvoreControle menu = new ArvoreControle();
 		menu.ExibeMenu();
 	}
